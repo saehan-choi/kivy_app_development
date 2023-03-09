66e2e557f16ca8300de4e6efa4636e05b3c20d25
unknown <saehanchoi>
2023-03-09 15:02:40 +0900

    test

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
-from kivy.network.urlrequest import UrlRequest
+# from kivy.network.urlrequest import UrlRequest
 
-
+import requests
 import time
 
 # 밑으로 전송함.
-server_address = '192.111.0.0'
-port = '8000'
-url = 'http://{}:{}'.format(server_address, port)
+url = 'http://127.0.0.1:8000/post'
 
 # window. 안먹히네 흠
 Window.maximize()
@@ -57,6 +55,8 @@ Builder.load_string('''
 
 # 나중에 필요한것들은 주석으로 달아놓았습니다.
 
+
+# 나중에 서버 열리면 이거 이용해서 포트 열어놓으면 됨 ; 씨빠꺼
 # def texture_to_image(texture):
 #     buf = texture.pixels
 #     size = texture.size
@@ -75,12 +75,27 @@ class CameraClick(BoxLayout):
         '''
         camera = self.ids['camera']
         timestr = time.strftime("%Y%m%d_%H%M%S")
-        
-        
+
         # img = texture_to_image(camera.texture)  -> 나중에 저 함수를 서버측에서 처리하도록 설정하기.
-        req = UrlRequest(url, req_body={'image': camera.texture}, on_success=self.on_success, on_failure=self.on_failure)
-        
-        
+        # req = UrlRequest(url, req_body={'image': camera.texture}, on_success=self.on_success, on_failure=self.on_failure)
+
+        # req = UrlRequest(url, req_body={'pixels': camera.texture.pixels,
+        #                                 'size' : camera.texture.size,
+        #                                 'mode' : camera.texture.colorfmt}, 
+        # 나중에 이거 추가하고 테스트 해보기
+
+
+        # requests에 타임아웃 지정해놓으면 그 시간대로 안들어오면 앱이 꺼집니다.
+        # 또한, 서버를 열어놔야 post신호를 받을수있어서 앱이 안꺼집니다.. 이건 WIFI 연결되면 할것
+        # response = requests.post(url, 
+        #                         json={
+        #                             'camera.texture': f'{camera.texture}',
+        #                             'camera.size': camera.size,
+        #                             'camera.colorfmt':camera.size
+        #                             }, 
+        #                         timeout=1)
+
+
         camera.export_to_png("IMG_{}.png".format(timestr))
         print("Captured")
 
